a06b966bb4f1aa2483148bf6ab9b1868a780148d
LadyVamp <ladyvamp666@gmail.com>
2022-09-05 22:27:45 +0300

    add categories and icons

---
+++ src/components/IconCategory.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <v-icon color="accent" :title="getIconLabel(category)">
+            {{ getIcon(category) }}
+        </v-icon>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IconCategory',
+    props: {
+        category: {
+            type: String,
+            required: true,
+            default: 'all',
+        },
+    },
+    data() {
+        return {
+            iconCategories: [
+                {
+                    key: 'all',
+                    label: 'Все категории',
+                    icon: 'mdi-layers-outline',
+                },
+                {
+                    key: 'repair',
+                    label: 'Дом и ремонт',
+                    icon: 'mdi-hammer-screwdriver',
+                },
+                {
+                    key: 'appliances',
+                    label: 'Электроника и бытовая техника',
+                    icon: 'mdi-kettle',
+                },
+                {
+                    key: 'universal',
+                    label: 'Универсальный',
+                    icon: 'mdi-shape',
+                },
+            ],
+        };
+    },
+
+    methods: {
+        getIcon(category) {
+            return this.iconCategories.find((item) => item.key === category).icon;
+        },
+        getIconLabel(category) {
+            return this.iconCategories.find((item) => item.key === category).label;
+        },
+    },
+};
+</script>
--- src/components/SearchAssistant.vue
+++ src/components/SearchAssistant.vue
@@ -15,15 +15,17 @@
         <v-row>
             <v-col>
                 <v-switch v-model="isSelectedAll" :disabled="!search" label="Выбрать все" @click="selectAll"></v-switch>
-                <v-checkbox
-                    v-for="(item, index) in shops"
-                    :key="index"
-                    v-model="item.selected"
-                    :label="item.label"
-                    :disabled="!search || isSelectedAll"
-                    @change="onChange(item)"
-                >
-                </v-checkbox>
+                <div v-for="(item, index) in shops" :key="index" class="d-flex">
+                    <IconCategory :category="item.category" class="icon-category" />
+                    <v-checkbox
+                        :key="index"
+                        v-model="item.selected"
+                        :label="item.label"
+                        :disabled="!search || isSelectedAll"
+                        @change="onChange(item)"
+                    >
+                    </v-checkbox>
+                </div>
             </v-col>
             <v-col class="links mt-6">
                 <div v-for="(item, index) in selectedShops" :key="index" class="mb-1">
@@ -42,9 +44,13 @@
 
 <script>
 import { detectIncognito } from 'detect-incognito';
+import IconCategory from '@/components/IconCategory';
 
 export default {
     name: 'SearchAssistant',
+    components: {
+        IconCategory,
+    },
     data() {
         return {
             search: '',
@@ -56,61 +62,73 @@ export default {
                     label: 'Все инструменты',
                     value: 'vseinstrumenti',
                     link: 'https://www.vseinstrumenti.ru/search_main.php?what=опахало',
+                    category: 'repair',
                 },
                 {
                     label: 'Леруа',
                     value: 'leroymerlin',
                     link: 'https://leroymerlin.ru/search/?q=опахало',
+                    category: 'repair',
                 },
                 {
                     label: '220 вольт',
                     value: '220-volt',
                     link: 'https://www.220-volt.ru/?digiSearch=true&term=опахало&params=%7Csort%3DPRICE_ASC',
+                    category: 'repair',
                 },
                 {
                     label: 'Восток-сервис',
                     value: 'vostok',
                     link: 'https://vostok.ru/search/catalog/?query=опахало',
+                    category: 'repair',
                 },
                 {
                     label: 'МВидео',
                     value: 'mvideo',
                     link: 'https://www.mvideo.ru/product-list-page?q=опахало',
+                    category: 'appliances',
                 },
                 {
                     label: 'Эльдорадо',
                     value: 'eldorado',
                     link: 'https://www.eldorado.ru/search/catalog.php?q=опахало',
+                    category: 'appliances',
                 },
                 {
                     label: 'DNS',
                     value: 'dns',
                     link: 'https://www.dns-shop.ru/search/?q=опахало',
+                    category: 'appliances',
                 },
                 {
                     label: 'Плеер',
                     value: 'pleer',
                     link: 'https://www.pleer.ru/search_опахало.html',
+                    category: 'appliances',
                 },
                 {
                     label: 'Холодильник',
                     value: 'holodilnik',
                     link: 'https://www.holodilnik.ru/search/?search=опахало&sort=hru',
+                    category: 'appliances',
                 },
                 {
                     label: 'Wildberries',
                     value: 'Wildberries',
                     link: 'https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search=опахало',
+                    category: 'universal',
                 },
                 {
                     label: 'Ozon',
                     value: 'Ozon',
                     link: 'https://www.ozon.ru/search/?text=опахало&from_global=true',
+                    category: 'universal',
                 },
                 {
                     label: 'Сбермаркет',
                     value: 'sbermarket',
                     link: 'https://sbermarket.ru/auchan/search?keywords=опахало&sid=177',
+                    category: 'universal',
                 },
             ],
         };
@@ -154,5 +172,9 @@ export default {
 }
 .links {
     margin-top: -12px;
+}
+.icon-category {
+    margin-right: 5px;
+    margin-top: -14px;
 }
 </style>
