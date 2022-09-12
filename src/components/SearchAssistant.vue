b248a0ec45a651bed0f040730b7738b1ef1af4f9
LadyVamp <ladyvamp666@gmail.com>
2022-09-12 22:56:31 +0300

    sort by price asc

--- src/components/SearchAssistant.vue
+++ src/components/SearchAssistant.vue
@@ -1,6 +1,8 @@
 <template>
     <v-container fluid>
-        <v-btn small @click="search = 'плейсмат'">плейсмат</v-btn>
+        <v-btn small @click="search = 'перчатки'">перчатки</v-btn>
+        <v-btn small class="mx-1" @click="search = 'noname'">noname</v-btn>
+        <v-btn small @click="search = 'сито'">сито</v-btn>
         <v-row>
             <v-col cols="6">
                 <v-text-field
@@ -15,6 +17,7 @@
         <v-row>
             <v-col>
                 <v-switch v-model="isSelectedAll" :disabled="!search" label="Выбрать все" @click="selectAll"></v-switch>
+                <v-switch v-model="isSortByPriceAsc" :disabled="!search" label="Sort by price asc"></v-switch>
                 <div v-for="(item, index) in shops" :key="index" class="d-flex">
                     <IconCategory :category="item.category" class="icon-category" />
                     <v-checkbox
@@ -56,96 +59,105 @@ export default {
             search: '',
             isIncognito: false,
             isSelectedAll: false,
+            isSortByPriceAsc: false,
             selectedShops: [],
             shops: [
                 {
                     label: 'Все инструменты',
                     value: 'vseinstrumenti',
-                    link: 'https://www.vseinstrumenti.ru/search_main.php?what=опахало',
+                    link: 'https://www.vseinstrumenti.ru/search_main.php?what=opahalo',
+                    link2: 'https://www.vseinstrumenti.ru/search_main.php?what=opahalo&orderby=price',
                     category: 'repair',
                 },
                 {
                     label: 'Леруа',
                     value: 'leroymerlin',
-                    link: 'https://leroymerlin.ru/search/?q=опахало',
-                    category: 'repair',
-                },
-                {
-                    label: '220 вольт',
-                    value: '220-volt',
-                    link: 'https://www.220-volt.ru/?digiSearch=true&term=опахало&params=%7Csort%3DPRICE_ASC',
+                    link: 'https://leroymerlin.ru/search/?q=opahalo',
+                    link2: 'https://leroymerlin.ru/search/?q=opahalo&sortby=1',
                     category: 'repair',
                 },
                 {
                     label: 'Восток',
                     value: 'vostok',
-                    link: 'https://vostok.ru/search/catalog/?query=опахало',
+                    link: 'https://vostok.ru/search/catalog/?query=opahalo',
+                    link2: 'https://vostok.ru/search/catalog/?query=opahalo&order=price_asc',
                     category: 'equipment',
                 },
                 {
                     label: 'Сплав',
                     value: 'splav',
-                    link: 'https://www.splav.ru/search/?q=опахало',
+                    link: 'https://www.splav.ru/search/?q=opahalo',
+                    link2: 'https://www.splav.ru/search/?q=opahalo&sort=price&order=asc',
                     category: 'equipment',
                 },
                 {
                     label: 'МВидео',
                     value: 'mvideo',
-                    link: 'https://www.mvideo.ru/product-list-page?q=опахало',
+                    link: 'https://www.mvideo.ru/product-list-page?q=opahalo',
+                    link2: 'https://www.mvideo.ru/product-list-page?q=opahalo&sort=price_asc',
                     category: 'appliances',
                 },
                 {
                     label: 'Эльдорадо',
                     value: 'eldorado',
-                    link: 'https://www.eldorado.ru/search/catalog.php?q=опахало',
+                    link: 'https://www.eldorado.ru/search/catalog.php?q=opahalo',
+                    link2: 'https://www.eldorado.ru/search/catalog.php?q=opahalo&sort=price&type=ASC',
                     category: 'appliances',
                 },
                 {
                     label: 'DNS',
                     value: 'dns',
-                    link: 'https://www.dns-shop.ru/search/?q=опахало',
+                    link: 'https://www.dns-shop.ru/search/?q=opahalo',
+                    link2: 'https://www.dns-shop.ru/search/?q=opahalo&order=price-asc',
                     category: 'appliances',
                 },
                 {
                     label: 'Плеер',
                     value: 'pleer',
-                    link: 'https://www.pleer.ru/search_опахало.html',
+                    link: 'https://www.pleer.ru/search_opahalo.html',
+                    link2: 'https://www.pleer.ru/search_opahalo_1-0,2-0,7-(),3-(),4-(),5-0,8-0,9-0,6-3',
                     category: 'appliances',
                 },
                 {
                     label: 'Холодильник',
                     value: 'holodilnik',
-                    link: 'https://www.holodilnik.ru/search/?search=опахало&sort=hru',
+                    link: 'https://www.holodilnik.ru/search/?search=opahalo',
+                    link2: 'https://www.holodilnik.ru/search/?search=opahalo&sort=price',
                     category: 'appliances',
                 },
                 {
                     label: 'Wildberries',
                     value: 'Wildberries',
-                    link: 'https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search=опахало',
+                    link: 'https://www.wildberries.ru/catalog/0/search.aspx?search=opahalo',
+                    link2: 'https://www.wildberries.ru/catalog/0/search.aspx?search=noname&sort=priceup',
                     category: 'universal',
                 },
                 {
                     label: 'Ozon',
                     value: 'Ozon',
-                    link: 'https://www.ozon.ru/search/?text=опахало&from_global=true',
+                    link: 'https://www.ozon.ru/search/?text=opahalo',
+                    link2: 'https://www.ozon.ru/search/?sorting=ozon_card_price&text=opahalo',
                     category: 'universal',
                 },
                 {
                     label: 'СберМегаМаркет',
                     value: 'sbermegamarket',
-                    link: 'https://sbermegamarket.ru/catalog/?q=опахало',
+                    link: 'https://sbermegamarket.ru/catalog/?q=opahalo',
+                    link2: 'https://sbermegamarket.ru/catalog/?q=opahalo#?sort=1',
                     category: 'universal',
                 },
                 {
                     label: 'СберМаркет',
                     value: 'sbermarket',
-                    link: 'https://sbermarket.ru/auchan/search?keywords=опахало&sid=177',
+                    link: 'https://sbermarket.ru/auchan/search?keywords=opahalo&sid=177',
+                    link2: 'https://sbermarket.ru/auchan/search?keywords=opahalo&sid=177&sort=price_asc',
                     category: 'universal',
                 },
                 {
                     label: 'Aliexpress',
                     value: 'aliexpress',
-                    link: 'https://aliexpress.ru/wholesale?SearchText=опахало&isFreeShip=y&SortType=total_tranpro_desc',
+                    link: 'https://aliexpress.ru/wholesale?SearchText=opahalo&isFreeShip=y&SortType=total_tranpro_desc',
+                    link2: 'https://aliexpress.ru/wholesale?SearchText=opahalo&isFreeShip=y&SortType=price_asc',
                     category: 'universal',
                 },
             ],
@@ -171,10 +183,14 @@ export default {
             this.selectedShops = [...new Set(this.selectedShops)];
         },
         productSearchLink(item) {
-            return item.link.replace('опахало', this.search);
+            if (this.isSortByPriceAsc) {
+                return item.link2.replace('opahalo', this.search);
+            } else {
+                return item.link.replace('opahalo', this.search);
+            }
         },
         openLinks() {
-            const links = this.selectedShops.map((item) => item.link.replace('опахало', this.search));
+            const links = this.selectedShops.map((item) => item.link.replace('opahalo', this.search));
             links.forEach((link) => {
                 window.open(link, '_blank');
             });
